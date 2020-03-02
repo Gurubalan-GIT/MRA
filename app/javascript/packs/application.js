9e5fee0ed88aa3e0dc2af7428935447571974ff5
Gurubalan-GIT <gurubalan.work@gmail.com>
2020-03-02 22:26:37 +0530

    fix: changes for select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +15,16 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import $ from 'jquery'
+import 'select2'
+import 'select2/dist/css/select2.css'
+
+$(document).on('turbolinks:load', function(){
+    // Turn on js-selectable class so that it becomes SELCT 2 tag
+    $('.js-selectable').select2({
+      allowClear: true,
+      width: 200
+      // If you are using Bootstrap, please add　`theme: "bootstrap"` too.
+    });
+  });
